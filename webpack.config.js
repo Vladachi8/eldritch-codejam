161fc70e6e737cd204618fc3e7a30dbaf2c6ca18
Vlad Choporov <Vladachi8@gmail.com>
2022-08-28 21:23:34 +0300

    add new options for config to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, 'dist'),
     publicPath: "/",
+    publicPath: "/eldritch-codejam/",
     //assetModuleFileName: 'assets/[name]',
   },
   module: {
